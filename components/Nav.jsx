40712aee30ebbcfd71af8dd6d10be80b4f8f9d53
ramis-khasianov <ramis.khasianov@gmail.com>
2023-05-08 18:52:14 +0500

    boilerplate for components

---
+++ components/Nav.jsx
@@ -0,0 +1,78 @@
+"use client"
+
+import Link from 'next/link';
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    }
+
+    setProviders();
+  }, []);
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image 
+          src="/assets/images/logo.svg"
+          alt="Promptopia logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Proomptopia</p>
+      </Link>
+
+      {/* Mobile */}
+      <div className="sm:flex hidden">
+          {isUserLoggedIn ? (
+            <div className="flex gap-3 md:gap-5">
+              <Link href="/create-promt" className="black_btn">
+                Create Post
+              </Link>
+
+              <button type="button" onClick={signOut} className="outline_btn">
+                Sign Out
+              </button>
+
+              <Link href="/profile" className="flex gap-2 flex-center">
+                <Image
+                  src="/assets/images/logo.svg"
+                  width={37}
+                  height={37}
+                  className="rounded-full"
+                  alt="Profile"
+                />
+              </Link>
+            </div>
+          ) : ( 
+            <>
+             {providers && Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  
+                </button>
+              ))}
+
+            </>
+          )}
+
+        </div>
+    </nav>
+  )
+}
+
+export default Nav

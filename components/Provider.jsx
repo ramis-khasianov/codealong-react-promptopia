058c65c2fbb8a7037d1cc019bb8c3f252279d94f
ramis-khasianov <ramis.khasianov@gmail.com>
2023-05-08 19:19:30 +0500

    auth provider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+"use client";
 
-const Provider = () => {
-  return (
-    <div>Provider</div>
+import { SessionProvider } from 'next-auth/react';
+
+const Provider = ({ children, session }) => {
+  return ( 
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
   )
 }
 

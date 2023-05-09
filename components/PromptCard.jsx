0e78285ca116d060fc6620abb9c2f23167867a70
ramis-khasianov <ramis.khasianov@gmail.com>
2023-05-09 17:54:12 +0500

    edit of prompt

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,7 +6,10 @@ import { useSession } from 'next-auth/react'
 import { usePathname, useRouter } from "next/navigation"
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-  const [copied, setCopied] = useState(false)
+  const [copied, setCopied] = useState(false);
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
   const handleCopy = () => {
     setCopied(post.prompt)
@@ -44,6 +47,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             }
             width={12}
             height={12}
+            alt="creator-image"
           />
         </div>
       </div>
@@ -57,6 +61,27 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+
+      {
+        session?.user.id === post.creator._id 
+        && pathName === '/profile'
+        && (
+          <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+            <p
+              className="font-inter text-sm green_gradient cursor-pointer"
+              onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p
+              className="font-inter text-sm orange_gradient cursor-pointer"
+              onClick={handleDelete}
+            >
+              Delete
+            </p>
+          </div>
+        )
+      }
     </div>
   )
 }

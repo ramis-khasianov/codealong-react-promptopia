12206f88d424c023595f678daf1aca8be5d4cc9e
ramis-khasianov <ramis.khasianov@gmail.com>
2023-05-09 18:03:18 +0500

    delete frontend

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,6 +5,7 @@ import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation';
 
 import Profile from '@components/Profile';
+import { set } from "mongoose";
 
 const MyProfile = () => {
     const router = useRouter();
@@ -25,7 +26,21 @@ const MyProfile = () => {
     }
 
     const handleDelete = async (post) => {
-        
+        const hasConfirmed = confirm('Are you sure you want to delete this prompt?');
+
+        if(hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`, {
+                    method: 'DELETE'
+                });
+
+                const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+                setPosts(filteredPosts);
+            } catch(error) {
+                console.log(error);
+            }
+        }
     }
 
     return (
